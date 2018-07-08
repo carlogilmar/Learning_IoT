c98c2fa8e226ea963125df830e2d795c9c4acd24
carlogilmar <carlogilmar12@gmail.com>
2018-07-08 17:22:07 -0500

    Fixing error in presence handle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,8 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
 import {Socket, Presence} from "phoenix"
 
 let user = document.getElementById("user").innerText
-
 let socket = new Socket("/socket", {params: {user: user}})
 socket.connect()
 
@@ -49,20 +35,19 @@ let render = (presences) => {
 let room = socket.channel("room:lobby")
 
 room.on("presence_state", state => {
+  console.log("==================== > presence")
+  console.log(state)
   presences = Presence.syncState(presences, state)
   render(presences)
 })
 
-room.on("presences_diff", diff => {
+room.on("presence_diff", diff => {
+  console.log("==================== > diff")
+  console.log(diff)
   presences = Presence.syncDiff( presences, diff)
   render(presences)
 })
 
 room.join()
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
-//
+  .receive("ok", resp => { console.log("Joined to Example Channel!!", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
