7fb2fb5245392b1892d55fd0bbe41c410105978a
carlogilmar <carlogilmar12@gmail.com>
2018-07-08 15:31:02 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,9 +13,56 @@
 // to also remove its path from "config.paths.watched".
 import "phoenix_html"
 
+import {Socket, Presence} from "phoenix"
+
+let user = document.getElementById("user").innerText
+
+let socket = new Socket("/socket", {params: {user: user}})
+socket.connect()
+
+let presences = {}
+
+let formatedTimestamp = (Ts) => {
+  let date = new Date(Ts)
+  return date.toLocaleString()
+}
+
+let listBy = (user, {metas:metas}) => {
+  return {
+    user: user,
+    onlineAt: formatedTimestamp(metas[0].online_at)
+  }
+}
+
+let userList = document.getElementById("userList")
+let render = (presences) => {
+  userList.innerHTML = Presence.list(presences, listBy)
+    .map(presence => `
+      <li>
+        <b>${presence.user}</b>
+        <br><small>online since ${presence.onlineAt}</small>
+      </li>
+    `)
+    .join("")
+}
+
+let room = socket.channel("room:lobby")
+
+room.on("presence_state", state => {
+  presences = Presence.syncState(presences, state)
+  render(presences)
+})
+
+room.on("presences_diff", diff => {
+  presences = Presence.syncDiff( presences, diff)
+  render(presences)
+})
+
+room.join()
 // Import local files
 //
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+//
