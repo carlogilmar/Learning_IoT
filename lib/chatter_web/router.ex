99aeea1a9d6f6c0075ff27b5ab6d8d4022cc8e8d
carlogilmar <carlogilmar12@gmail.com>
2018-07-08 22:35:52 -0500

    Adding a resource for show a new view

--- lib/chatter_web/router.ex
+++ lib/chatter_web/router.ex
@@ -15,8 +15,8 @@ defmodule ChatterWeb.Router do
 
   scope "/", ChatterWeb do
     pipe_through :browser # Use the default browser stack
-
     get "/", PageController, :index
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
