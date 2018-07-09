46a63d25e17200b1baf69044d9c33f1695044c0a
carlogilmar <carlogilmar12@gmail.com>
2018-07-08 19:33:37 -0500

    Adding mexican debuggers

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,30 @@ import "phoenix_html"
 
 import {Socket, Presence} from "phoenix"
 
+// Making the socket connection
 let user = document.getElementById("user").innerText
 let socket = new Socket("/socket", {params: {user: user}})
 socket.connect()
 
+// Join and open the socket channels
+let room = socket.channel("room:lobby")
+room.on("presence_state", state => {
+  console.log("==================== > presence")
+  console.log(state)
+  presences = Presence.syncState(presences, state)
+  render(presences)
+})
+room.on("presence_diff", diff => {
+  console.log("==================== > diff")
+  console.log(diff)
+  presences = Presence.syncDiff( presences, diff)
+  render(presences)
+})
+room.join()
+  .receive("ok", resp => { console.log("Joined to Example Channel!!", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+// Getting the list of users
 let presences = {}
 
 let formatedTimestamp = (Ts) => {
@@ -32,22 +52,3 @@ let render = (presences) => {
     .join("")
 }
 
-let room = socket.channel("room:lobby")
-
-room.on("presence_state", state => {
-  console.log("==================== > presence")
-  console.log(state)
-  presences = Presence.syncState(presences, state)
-  render(presences)
-})
-
-room.on("presence_diff", diff => {
-  console.log("==================== > diff")
-  console.log(diff)
-  presences = Presence.syncDiff( presences, diff)
-  render(presences)
-})
-
-room.join()
-  .receive("ok", resp => { console.log("Joined to Example Channel!!", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
--- lib/chatter_web/channels/room_channel.ex
+++ lib/chatter_web/channels/room_channel.ex
@@ -2,17 +2,31 @@ defmodule ChatterWeb.RoomChannel do
 
   use ChatterWeb, :channel
   alias ChatterWeb.Presence
+  require Logger
 
   def join("room:lobby", _, socket) do
+    Logger.info ":: Join to room:lobby ::", ansi_color: :green
+    IO.inspect socket
     send self(), :after_join
     {:ok, socket}
   end
 
   def handle_info(:after_join, socket) do
+
+    Logger.info ":: New Connection Interaction ::", ansi_color: :green
+    IO.inspect socket
+
+    # socker.assigns is a map: %{user: "Carlo2 "}
+    Logger.info ":: Updating Presence track with the new user ::"
     Presence.track( socket, socket.assigns.user, %{
       online_at: :os.system_time(:milli_seconds)
     })
-    push socket, "presence_state", Presence.list(socket)
+
+    presence_list = Presence.list(socket)
+    IO.inspect presence_list
+
+    # presence_state lives in js app
+    push socket, "presence_state", presence_list
     {:noreply , socket}
   end
 
