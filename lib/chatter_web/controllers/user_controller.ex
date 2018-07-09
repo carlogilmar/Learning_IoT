5f4db51250233c617f349ce4d4d96788fe2f5c41
carlogilmar <carlogilmar12@gmail.com>
2018-07-09 00:34:38 -0500

    Adding functions for update

--- lib/chatter_web/controllers/user_controller.ex
+++ lib/chatter_web/controllers/user_controller.ex
@@ -6,6 +6,7 @@ defmodule ChatterWeb.UserController do
   alias Chatter.Repo
   require Logger
 
+  # Show all users
   def index(conn, _params) do
     # Getting all users from database
     users = Repo.all(User)
@@ -21,12 +22,14 @@ defmodule ChatterWeb.UserController do
     render(conn, "show.html", user: user)
   end
 
+  # Create view
   def new(conn, _params) do
     Logger.info ":: Form for New User ::", ansi_color: :yellow
     changeset = User.changeset(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
+  # Creating action
   def create(conn, %{"user" => user_params}) do
     Logger.info ":: Creating a new user ::", ansi_color: :yellow
     IO.inspect user_params
@@ -40,6 +43,27 @@ defmodule ChatterWeb.UserController do
           |> redirect(to: user_path(conn, :index) )
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  # Update view
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, %{})
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  # Updating action
+  def update( conn, %{"id" => id, "user" => user_params }) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user, user_params)
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+          |> put_flash(:info, "User Updated!")
+          |> redirect(to: user_path(conn, :show, user) )
+      {:error, changeset} ->
+        render(conn, "edit.html", user: user, changeset: changeset)
     end
   end
 
