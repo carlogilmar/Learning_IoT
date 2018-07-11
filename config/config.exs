378b13ca4ad76d6c2aa97b40b6db897cd8167add
carlogilmar <carlogilmar12@gmail.com>
2018-07-11 01:02:14 -0500

    Adding guardian configuration, with secret generated

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,16 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
+config :guardian, Guardian,
+  allowed_algos: ["HS512"],
+  verify_module: Guardian.JWT,
+  issuer: "Chatter",
+  ttl: { 30, :days },
+  allowed_drift: 2000,
+  verify_issuer: true,
+  secret_key: "ry6n7iqYCZJshjW3leuBAUbnMDVfvk1Ao2I3sYqdCy7tNoDr7rBg6yl2YScUeEXB",
+  serializer: Chatter.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
