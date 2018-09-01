5db95d4f28e7c02aee4850e137d3099cac8f537e
carlogilmar <carlogilmar12@gmail.com>
2018-08-31 23:44:55 -0500

    Adding the song in server for assign from genserver

--- lib/chatter/director.ex
+++ lib/chatter/director.ex
@@ -2,8 +2,16 @@ defmodule Chatter.Director do
 
   use GenServer
 
-  @songs [ "song1", "song2", "song3", "song4",
-           "song5", "song6", "song7", "song8"]
+  @songs [
+    %{:song => "http://carlogilmar.me/uno.m4a", :category => "A"},
+    %{:song => "http://carlogilmar.me/dos.m4a", :category => "B"},
+    %{:song => "http://carlogilmar.me/tres.m4a", :category => "C"},
+    %{:song => "http://carlogilmar.me/cuatro.m4a", :category => "D"},
+    %{:song => "http://carlogilmar.me/cinco.m4a", :category => "E"},
+    %{:song => "http://carlogilmar.me/seis.m4a", :category => "F"},
+    %{:song => "http://carlogilmar.me/siete.m4a", :category => "G"},
+    %{:song => "http://carlogilmar.me/ocho.m4a", :category => "H"}
+  ]
 
   def start_link() do
     GenServer.start_link(__MODULE__, [], [name: __MODULE__])
