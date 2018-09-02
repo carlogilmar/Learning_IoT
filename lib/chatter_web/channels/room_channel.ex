f0ef45ba9e0fedfa2aec23be43075cfdae96d6af
carlogilmar <carlogilmar12@gmail.com>
2018-09-01 22:54:33 -0500

    Sending something when join a socket channel

--- lib/chatter_web/channels/room_channel.ex
+++ lib/chatter_web/channels/room_channel.ex
@@ -9,35 +9,35 @@ defmodule ChatterWeb.RoomChannel do
     Logger.info ":: Join to room:lobby ::", ansi_color: :green
     { _, current_user} = Director.suscribe()
     uart = Chatter.Uart.get_uart()
-    Nerves.UART.write(uart, "2")
+    Nerves.UART.write(uart, "1")
     send self(), :after_join
     {:ok, current_user, socket}
   end
 
-  def handle_info(:after_join, socket) do
-    Presence.track( socket, "user", %{
-      online_at: :os.system_time(:milli_seconds)
-    })
-    presence_list = Presence.list(socket)
-    # presence_state lives in js app
-    push socket, "presence_state", presence_list
-    {:noreply , socket}
-  end
+  #def handle_info(:after_join, socket) do
+  #  Presence.track( socket, "user", %{
+  #    online_at: :os.system_time(:milli_seconds)
+  #  })
+  #  presence_list = Presence.list(socket)
+  #  # presence_state lives in js app
+  #  push socket, "presence_state", presence_list
+  #  {:noreply , socket}
+  #end
 
-  def handle_in("room::sync", message, socket) do
-    IO.puts "Alguien vino o alguien se fue !!!"
-    {:noreply, socket}
-  end
+  #def handle_in("room::sync", message, socket) do
+  #  IO.puts "Alguien vino o alguien se fue !!!"
+  #  {:noreply, socket}
+  #end
 
-  def handle_in("message:new", message, socket) do
-    broadcast! socket, "message:new:client", %{
-      user: socket.assigns.user,
-      body: message,
-      timestamp: :os.system_time(:milli_seconds)
-    }
-    send_to_arduino( message )
-    {:noreply, socket}
-  end
+  #def handle_in("message:new", message, socket) do
+  #  broadcast! socket, "message:new:client", %{
+  #    user: socket.assigns.user,
+  #    body: message,
+  #    timestamp: :os.system_time(:milli_seconds)
+  #  }
+  #  send_to_arduino( message )
+  #  {:noreply, socket}
+  #end
 
   def send_message_from_terminal(body) do
     ChatterWeb.Endpoint.broadcast "room:lobby", "message:new:client", %{body: body}
