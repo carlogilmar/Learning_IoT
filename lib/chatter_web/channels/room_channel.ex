15aecb00e86bc18d10bb0329ae36abba292b67b5
carlogilmar <carlogilmar12@gmail.com>
2018-09-01 23:10:16 -0500

    Adding fucntions fot get the message from arduino uart

--- lib/chatter_web/channels/room_channel.ex
+++ lib/chatter_web/channels/room_channel.ex
@@ -14,61 +14,22 @@ defmodule ChatterWeb.RoomChannel do
     {:ok, current_user, socket}
   end
 
-  #def handle_info(:after_join, socket) do
-  #  Presence.track( socket, "user", %{
-  #    online_at: :os.system_time(:milli_seconds)
-  #  })
-  #  presence_list = Presence.list(socket)
-  #  # presence_state lives in js app
-  #  push socket, "presence_state", presence_list
-  #  {:noreply , socket}
-  #end
-
-  #def handle_in("room::sync", message, socket) do
-  #  IO.puts "Alguien vino o alguien se fue !!!"
-  #  {:noreply, socket}
-  #end
-
-  #def handle_in("message:new", message, socket) do
-  #  broadcast! socket, "message:new:client", %{
-  #    user: socket.assigns.user,
-  #    body: message,
-  #    timestamp: :os.system_time(:milli_seconds)
-  #  }
-  #  send_to_arduino( message )
-  #  {:noreply, socket}
-  #end
-
   def send_message_from_terminal(body) do
     ChatterWeb.Endpoint.broadcast "room:lobby", "message:new:client", %{body: body}
   end
 
-  def send_to_arduino( message ) do
-    IO.puts "Enviando al arduino..."
-    uart = Chatter.Uart.get_uart()
-    IO.inspect uart
-    IO.puts "Enviando..."
-    manage_led( message, uart )
-  end
-
-  def manage_led("1", pid) do
-    IO.puts "Prende"
-    Nerves.UART.write(pid, "1")
-    :timer.sleep(500);
-    IO.puts "Apaga"
-    Nerves.UART.write(pid, "0")
-    :timer.sleep(500);
-    IO.puts "Prende"
-    Nerves.UART.write(pid, "1")
-    :timer.sleep(500);
-    IO.puts "Apaga"
-    Nerves.UART.write(pid, "0")
-    :timer.sleep(500);
-    IO.puts "Prende"
-    Nerves.UART.write(pid, "1")
-    :timer.sleep(500);
-  end
-
-  def manage_led(_, pid), do: Nerves.UART.write(pid, "0")
+  def send_broadcast( "play" ), do: IO.puts(" Play !")
+  def send_broadcast( "stop" ), do: IO.puts(" stop ")
+  def send_broadcast( "49" ), do: IO.puts(" song 1 ")
+  def send_broadcast( "50" ), do: IO.puts(" song 2 ")
+  def send_broadcast( "51" ), do: IO.puts(" song 3 ")
+  def send_broadcast( "52" ), do: IO.puts(" song 4 ")
+  def send_broadcast( "53" ), do: IO.puts(" song 5 ")
+  def send_broadcast( "54" ), do: IO.puts(" song 6 ")
+  def send_broadcast( "55" ), do: IO.puts(" song 7 ")
+  def send_broadcast( "56" ), do: IO.puts(" song 8 ")
+  def send_broadcast( "65" ), do: IO.puts(" play 2!! ")
+  def send_broadcast( "66" ), do: IO.puts(" stop 2!! ")
+  def send_broadcast( "67" ), do: IO.puts(" RELOAD!!")
 
 end
