0f14fd2ef445cc609a2b2e1fb4e5ee9f86d17ad9
carlogilmar <carlogilmar12@gmail.com>
2018-09-02 01:07:42 -0500

    Implement case instead of functions

--- lib/chatter_web/channels/room_channel.ex
+++ lib/chatter_web/channels/room_channel.ex
@@ -8,30 +8,54 @@ defmodule ChatterWeb.RoomChannel do
   def join("room:lobby", _, socket) do
     Logger.info ":: Join to room:lobby ::", ansi_color: :green
     { _, current_user} = Director.suscribe()
-    uart = Chatter.Uart.get_uart()
-    Nerves.UART.write(uart, "1")
     send self(), :after_join
     {:ok, current_user, socket}
   end
 
+  def play_pacman_in_arduino() do
+    uart = Chatter.Uart.get_uart()
+    Nerves.UART.write(uart, "1")
+  end
+
   def send_message_from_terminal(body) do
     IO.puts " :: Broadcast :: << #{body} >> "
+    Logger.info ":: Sending Broadcast Message!! ::", ansi_color: :yellow
     ChatterWeb.Endpoint.broadcast "room:lobby", "message:new:client", %{body: body}
   end
 
-  def send_broadcast( "play" ), do: send_message_from_terminal("start")
-  def send_broadcast( "stop" ), do: send_message_from_terminal("stop")
-  def send_broadcast( "49" ), do: send_message_from_terminal("A")
-  def send_broadcast( "50" ), do: send_message_from_terminal("B")
-  def send_broadcast( "51" ), do: send_message_from_terminal("C")
-  def send_broadcast( "52" ), do: send_message_from_terminal("D")
-  def send_broadcast( "53" ), do: send_message_from_terminal("E")
-  def send_broadcast( "54" ), do: send_message_from_terminal("F")
-  def send_broadcast( "55" ), do: send_message_from_terminal("G")
-  def send_broadcast( "56" ), do: send_message_from_terminal("H")
-  def send_broadcast( "65" ), do: send_message_from_terminal("start")
-  def send_broadcast( "66" ), do: send_message_from_terminal("stop")
-  def send_broadcast( "67" ), do: send_message_from_terminal("reload")
-  def send_broadcast( _ ), do: IO.puts(" Monitoring UART! ")
+  def send_broadcast( message ) do
+    case message do
+      "play" ->
+        send_message_from_terminal("start")
+      "stop" ->
+        send_message_from_terminal("stop")
+      "49" ->
+        send_message_from_terminal("A")
+      "50" ->
+        send_message_from_terminal("B")
+      "51" ->
+        send_message_from_terminal("C")
+      "52" ->
+        send_message_from_terminal("D")
+      "53" ->
+        send_message_from_terminal("E")
+      "54" ->
+        send_message_from_terminal("F")
+      "55" ->
+        send_message_from_terminal("G")
+      "56" ->
+        send_message_from_terminal("H")
+      "65" ->
+        send_message_from_terminal("start")
+      "66" ->
+        send_message_from_terminal("play")
+      "67" ->
+        send_message_from_terminal("stop")
+      "68" ->
+        send_message_from_terminal("reload")
+      _ ->
+        Logger.info ":: Monitoring UART ::", ansi_color: :green
+    end
+  end
 
 end
