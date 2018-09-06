1dd708b7a5009d890583efcbb35c8dbbea504b7e
carlogilmar <carlogilmar12@gmail.com>
2018-09-05 23:16:47 -0500

    Adding a token env path

--- config/config.exs
+++ config/config.exs
@@ -1,15 +1,7 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
-#config :chatter,
-#  ecto_repos: [Chatter.Repo]
+config :ex_gram, token: "here-put-the-token"
 
-# Configures the endpoint
 config :chatter, ChatterWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "O1cq7mKv5Ord7CoCUVG7HvCt8WiQew+xooQ5XmRmzkVUDPpO5qkPujzp1uzMG/V0",
@@ -17,11 +9,8 @@ config :chatter, ChatterWeb.Endpoint,
   pubsub: [name: Chatter.PubSub,
            adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
