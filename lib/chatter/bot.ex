67dc842445e5fbc0ab588857c876447d5ec259d8
carlogilmar <carlogilmar12@gmail.com>
2018-09-05 23:35:05 -0500

    Sending a broadcast message from telegram receiver

--- lib/chatter/bot.ex
+++ lib/chatter/bot.ex
@@ -4,15 +4,16 @@ defmodule Chatter.Bot do
 
   use ExGram.Bot, name: @bot
   require Logger
+  alias ChatterWeb.RoomChannel
 
   command("echo")
   middleware(ExGram.Middleware.IgnoreUsername)
 
-	def handle({:text, text, msg}, cnt) do
+	def handle({:text, text, _}, cnt) do
 		IO.puts "Bot respondiendo..."
 		IO.inspect text
-		IO.inspect msg
-		cnt |> answer(" Bot de Elixir desde Raspberrypi Respondiendo")
+    RoomChannel.send_broadcast text
+		cnt |> answer(" Aprendiendo IoT Primeros Pasos Demo:: Hola, ya te atiendo con Elixir 🤖")
 	end
 
   def handle({:command, :echo, %{text: t}}, cnt) do
